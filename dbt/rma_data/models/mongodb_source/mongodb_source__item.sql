c78b0b90bb11d50e8913df23efad0256c07122c1
Shaid Azmin <excel.azmin@gmail.com>
2023-01-28 16:25:24 +0600

    create dbt project & add sources to target dbt using sql

---
+++ dbt/rma_data/models/mongodb_source/mongodb_source__item.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table') }}
+
+with source_item as (
+    select 1 as id
+    union all
+    select null as id
+)
+select * from source_item
+
+-- final as (
+    
+-- )
+
+-- select * from  final
